f3486f2673e7acbc5db4ce83bbf157f33397c823
Alyshan Jahani <alyshanjahani@Alyshans-MacBook-Pro.local>
2019-08-08 21:16:30 -0400

    bug-fix - smtplib server.send_message() changed a bit

--- email_sender.py
+++ email_sender.py
@@ -3,8 +3,8 @@ import os
 import smtplib
 from email.message import EmailMessage
 
+msg = None
 content = sys.stdin.read()
-
 if content:
     msg = EmailMessage()
     msg.set_content(content)
@@ -12,18 +12,14 @@ else:
     print("No content")
     sys.exit()
 
-msg['Subject'] = 'The contents' 
-msg['From'] = ""
-msg['To'] = os.environ['RECIEVER']
-
+reciever = os.environ['RECIEVER']
 gmail_user = os.environ['GMAIL_USER']
 gmail_pass = os.environ['GMAIL_PASS']
 
-try:
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.login(gmail_user, gmail_pass)
-    server.send_message(msg)
-    server.close()
-    print("sent")
-except Exception as e:
-    print(e)
+msg['Subject'] = 'Footy Matches of Interest'
+
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server.login(gmail_user, gmail_pass)
+server.send_message(msg, from_addr=gmail_user, to_addrs=[reciever])
+server.close()
+print("sent")
